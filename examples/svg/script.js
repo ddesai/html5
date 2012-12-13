aa40acdaf8187fc0e352d082e64ae729e9525d36
Darshan <darshanbdesai@gmail.com>
2012-12-13 15:33:20 -0500

    Added support for using json

--- examples/svg/script.js
+++ examples/svg/script.js
@@ -2,7 +2,8 @@ function getName() {
     return "Darshan";
 }
 
-function toggleTheHighlight(){
+function toggleTheHighlight(click_event_info){
+    console.dir(click_event_info);
     $("h1").toggleClass("highlighted");
 }
 
@@ -13,8 +14,29 @@ function sliderValueChanged(info) {
     var slider_value = info.target.value;
     $("#left_eye").css("stroke-width",slider_value+"px");
     $("#right_eye").css("stroke-width",slider_value-5+"px");
-
+    $("#right_eye").css("stroke-width",slider_value-5+"px");
 }
+
 
 $("#my_btn").click(toggleTheHighlight);
 $("#my_slider").change(sliderValueChanged);
+
+/*
+    1) We listen for a click of a button
+    2) We are going to call a server (local)
+    3) We are going to get back an address object (in JSON format)
+    4) We are going to change the H1 on the page to say the city name
+*/
+
+function changeHeaderName(jsonResponse) {
+    console.dir(jsonResponse);
+    $("h1").html("I work at Intel in ");
+}
+
+
+function serverLookup() {
+    $.getJSON("address.json", changeHeaderName);
+}
+
+
+$("#btn1").click(serverLookup);
